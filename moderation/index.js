82b3dd4efe3c3b34ceddd91ed4cc6d49dec0f6fb
fkaaziebu <frederickaziebu1998@gmail.com>
2023-05-12 17:14:44 +0000

    Adding Comment Moderation

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,9 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {});
+app.post("/events", (req, res) => {
+  
+});
 
 app.listen(4003, () => {
   console.log("Listening on port 4003");
