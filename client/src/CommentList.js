e7c18f821a9925663b06bbc00c117cdca183f756
fkaaziebu <frederickaziebu1998@gmail.com>
2023-05-12 15:57:59 +0000

    Using the Query Service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,23 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { render } from "@testing-library/react";
+import React from "react";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
